1d947b49a9fc30a2c440ccac39b905c878782789
joel- <joel-@LAPTOP-DDTABC1H>
2024-01-27 15:10:54 -0600

    Added a line with a message

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -4,5 +4,6 @@ public class Message {
 
 	public static void main(String args[]) {
 		System.out.println("Say hi to your group mates!");
+		System.out.println("Howdy y'all, sorry I'm 2 weeks late. -Joel W.");
 	}
 }
